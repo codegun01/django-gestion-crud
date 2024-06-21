fd0899f0b869355cfb89dc3a2759dd8dbdfa4bc1
gonza <gonzaloriveros01@gmail.com>
2024-06-21 19:15:04 -0400

    tercer intento

--- gestionpacientes/settings.py
+++ gestionpacientes/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!or3ja1vyuvz$yim32s_g^41heom=lsl)y%*&+8m+&g7h2*_j3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
